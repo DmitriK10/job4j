ab21129312b56c21c18acd41a04a6f1e4c1de719
DmitriK10 <ymd@yandex.ru>
2018-03-09 17:49:29 +0300

    add calculator\Converter.java

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Converter.java
@@ -0,0 +1,31 @@
+package ru.job4j.calculator;
+
+/**
+ * Корвертор валюты.
+ * @author DKupriyanyuk
+ * @version $ld$
+ * @since 08.03.2018
+ */
+
+public class Converter {
+
+    /**
+     * Конвертируем рубли в евро.
+     * @param value рубли.
+     * @return Евро.
+     */
+    public int rubleToEuro(int value) {
+        value = value/70;
+        return value; //-1;
+    }
+
+    /**
+     * Конвертируем рубли в доллары.
+     * @param value рубли.
+     * @return Доллоры
+     */
+    public int rubleToDollar(int value) {
+        value = value/60;
+        return value;  //-1;
+    }
+}
