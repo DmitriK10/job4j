f3ad7143c516a860b592e77785ba2beb4dab4de5
DmitriK10 <ymd@yandex.ru>
2018-03-09 18:01:54 +0300

    add calculator\ConverterTest.java

---
+++ chapter_001/src/test/java/ru/job4j/calculator/ConverterTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * Class ConverterTest.
+ * @author DKupriyanyuk
+ * @version $ld$
+ * @since 08.03.2018
+ */
+public class ConverterTest {
+    @Test
+    public void when60RubleToDollarThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(60);
+        assertThat(result, is(1D));
+    }
+
+    @Test
+    public void when70RubleToEuroThen1() {
+        Converter converter = new Converter();
+        int result = converter.rubleToDollar(70);
+        assertThat(result, is(1D));
+    }
+}
