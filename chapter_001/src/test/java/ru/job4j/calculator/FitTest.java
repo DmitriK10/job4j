950b3c66372eed6762264685faacf0beef69b40c
DmitriK10 <ymd@yandex.ru>
2018-03-10 15:04:57 +0300

    add calculator\FitTest.java

---
+++ chapter_001/src/test/java/ru/job4j/calculator/FitTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class FitTest.
+ * @author DKupriyanyuk
+ * @version $ld$
+ * @since 10.03.2018
+ */
+
+public class FitTest {
+
+    @Test
+    public void manWeight() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(180);
+        assertThat(weight, is(92D));
+    }
+
+    @Test
+    public void womanWeight() {
+        Fit fit = new Fit();
+        double weight = fit.manWeight(170);
+        assertThat(weight, is(69D));
+    }
+}
