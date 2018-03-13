537eb52220b1d00082c2b64e58032ef782393c39
DmitriK10 <ymd@yandex.ru>
2018-03-13 15:45:29 +0300

    add main\java\ru\job4j\max\Max.java and test\java\ru\job4j\max\MaxTest.java

---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Class MaxTest.
+ * @author DKupriyanyuk
+ * @version $ld$
+ * @since 13.03.2018
+ */
+
+public class MaxTest {
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenSecondLessFirst() {
+        Max maxim = new Max();
+        int result = maxim.max(4, 2);
+        assertThat(result, is(1));
+    }
+}
