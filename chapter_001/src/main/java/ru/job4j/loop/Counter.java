418c235bb3dd6c1b3ad1aa98496a5d5b82eee11d
DmitriK10 <ymd@yandex.ru>
2018-03-22 19:50:01 +0300

    add main\java\ru\job4j\loop\Counter.java and test\java\ru\job4j\loop\CounterTest.java

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+/**
+ * @author Dmitry Kupriyanyuk (kupriyanyuk.d@yandex.ru)
+ * @version $Id$
+ * @since 21.03.18
+ */
+
+public class Counter {
+
+    /**
+     * Ищем сумму чисел в диапазоне от start до finish
+     * @param start первое число диапазона.
+     * @param finish последнее число диапазона.
+     * @return sum сумма.
+     */
+
+    public int add(int start, int finish) {
+        int sum = 0;   // сумма чисел
+        int i = start;
+
+        while (i <= finish)
+        {
+            if (i % 2 != 0)
+            {
+               sum += i;
+            }
+            i++;
+        }
+        return sum;
+    }
+}
