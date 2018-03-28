51bab270d1d016f4f11fad9be867644374698d8c
DmitriK10 <ymd@yandex.ru>
2018-03-28 21:15:20 +0300

    add main\java\ru\job4j\loop\Board.java and test\java\ru\job4j\loop\BoardTest.java

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,42 @@
+package ru.job4j.loop;
+
+/**
+ * @author Dmitry Kupriyanyuk (kupriyanyuk.d@yandex.ru)
+ * @version $Id$
+ * @since 26.03.18
+ */
+
+public class Board {
+
+    /**
+     * Строим шахматную доску символом Х по заданным: ширине и высоте
+     * @param width ширина игрового поля.
+     * @param height высота игрового поля.
+     * @return screen игровое поле в формате строки.
+     */
+
+    public static String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        String str = "Привет";
+        int nest = 0;
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                // условие проверки, что писать пробел или X
+                // Выше в задании мы определили закономерность, когда нужно проставлять X
+                if ((i + j) % 2 == 0)  {
+                    screen.append("X");
+                } else {
+                    screen.append(" ");
+                }
+                nest++;
+            }
+            // добавляем перевод на новую строку.
+            //System.out.print(text1);
+            screen.append(ln);
+        }
+
+        return screen.toString();
+    }
+
+}
