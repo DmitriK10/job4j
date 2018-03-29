3ab20e99f38df15bdaffbdb580276177ad1dd183
DmitriK10 <ymd@yandex.ru>
2018-03-29 15:31:57 +0300

    correct test value to 1 test\java\ru\job4j\calculator\ConverterTest.java

--- chapter_001/src/test/java/ru/job4j/calculator/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/ConverterTest.java
@@ -15,13 +15,13 @@ public class ConverterTest {
     public void when60RubleToDollarThen1() {
         Converter converter = new Converter();
         int result = converter.rubleToDollar(60);
-        assertThat(result, is(1D));
+        assertThat(result, is(1));
     }
 
     @Test
     public void when70RubleToEuroThen1() {
         Converter converter = new Converter();
         int result = converter.rubleToDollar(70);
-        assertThat(result, is(1D));
+        assertThat(result, is(1));
     }
 }
