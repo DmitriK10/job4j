01b1c99b3751e98c642ec9a1b0cef57d6c39fd2e
DmitriK10 <ymd@yandex.ru>
2018-03-30 20:42:01 +0300

    add main\java\ru\job4j\array\FindLoop.java and test\java\ru\job4j\array\FindLoopTest.java

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+/**
+ * @author Dmitry Kupriyanyuk (kupriyanyuk.d@yandex.ru)
+ * @version $Id$
+ * @since 30.03.18
+ */
+
+public class FindLoop {
+
+    /**
+     * Реализовать поиск значения в массиве.
+     * @param data массив чисел.
+     * @param el число поиска.
+     * @return rst результат поиска.
+     */
+
+    public int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int aData : data) {
+            //for (int i = 0; i < data.length; i++) {
+            if (aData == el) {
+                rst = el;
+                break;
+            }
+        }
+        return rst;
+    }
+}
