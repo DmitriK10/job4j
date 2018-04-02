b9630cb106d034a807c3411e490726c187c254c7
DmitriK10 <ymd@yandex.ru>
2018-04-02 22:54:48 +0300

    add main\java\ru\job4j\array\BubbleSort.java and test\java\ru\job4j\array\BubbleSortTest.java

---
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+
+/**
+ * @author Dmitry Kupriyanyuk (kupriyanyuk.d@yandex.ru)
+ * @version $Id$
+ * @since 02.04.18
+ */
+
+public class BubbleSort {
+    /**
+     * Реализовать сортировку значений по возрастанию в массиве.
+     * @param array массив чисел.
+     * @return array отсортированный массив.
+     */
+
+    public int[] bubblesort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+        return array;
+    }
+}
