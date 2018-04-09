0111f325585daea82da887d28376d8742b284786
DmitriK10 <ymd@yandex.ru>
2018-04-09 17:50:35 +0300

    add main\java\ru\job4j\array\Matrix.java and test\java\ru\job4j\array\MatrixTest.java

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * @author Dmitry Kupriyanyuk (kupriyanyuk.d@yandex.ru)
+ * @version $Id$
+ * @since 03.04.18
+ */
+
+
+public class Matrix {
+    /**
+     * Реализовать таблицу умножения.
+     * @param size размер таблицы.
+     * @return array умноженный массив.
+     */
+
+    public int[][] multiple(int size) {
+        int[][] array  = new int[size ][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                array[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return array;
+    }
+
+}
+
