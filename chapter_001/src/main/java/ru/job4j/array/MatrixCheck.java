e4209cbf2576bc2babce4997cd11cd9679d12d43
DmitriK10 <ymd@yandex.ru>
2018-07-23 19:24:37 +0300

    the algorithm is modified main\java\ru\job4j\array\MatrixCheck.java

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -16,14 +16,19 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         Boolean  symbLeftDiag = data[0][0];
         Boolean  symbRightDiag = data[data.length-1][0];
-        boolean toright, toleft;
-        toright = true;
-        toleft = true;
+        boolean res = true;
+        //toright, toleft;
+        //toright = true;
+        //toleft = true;
         for (int i = 0; i < data.length; i++) {
-            toright = toright & (data[i][i] == symbLeftDiag);
-            toleft &= (data[data.length-i-1][i] == symbRightDiag);
+            //toright = toright & (data[i][i] == symbLeftDiag);
+            if ((symbLeftDiag != data[i][i])||(symbRightDiag != data[data.length-i-1][i])) {
+                res = false;
+                break;
+            }
+            //toleft &= (data[data.length-i-1][i] == symbRightDiag);
         }
-
-        return toright & toleft;
+        return res;
+        //return toright & toleft;
     }
 }
