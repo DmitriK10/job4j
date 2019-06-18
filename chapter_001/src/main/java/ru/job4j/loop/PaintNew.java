cb5ac99dd1a8cce67983bc95685ccfa09774821d
Dmitri K <KomovDA@yandex.ru>
2019-06-18 18:39:40 +0300

    add PantNew.java and PantNewTest.Java решение задачи 5.4. Построить пирамиду в псевдографике.

---
+++ chapter_001/src/main/java/ru/job4j/loop/PaintNew.java
@@ -0,0 +1,55 @@
+package ru.job4j.loop;
+
+import java.util.StringJoiner;
+/**
+ * @author Dmitry Kupriyanyuk (kupriyanyuk.d@yandex.ru)
+ * @version $Id$
+ * @since 10.06.2019
+ */
+public class PaintNew {
+    public String rightTrl(int h) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int height = 0; height != h; height++) {
+            for (int width = 0; width != h*2-1; width++) {
+                if (height >= h- width - 1 && width <= height + h - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int width = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (row >= width- column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int width= 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != width; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+}
