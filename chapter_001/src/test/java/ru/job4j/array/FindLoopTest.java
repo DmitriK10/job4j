dfa9a19839a37db34429ccc7e3e5e8962941b101
Dmitri K <KomovDA@yandex.ru>
2019-10-06 21:12:49 +0300

    Findloop.java & FindLoiopTest.java возвращение оригинальных файлов измененных ошиблочно

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -1,9 +1,8 @@
-/*
 package ru.job4j.array;
 
-        import org.junit.Test;
-        import static org.hamcrest.core.Is.is;
-        import static org.junit.Assert.assertThat;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
 
@@ -26,25 +25,4 @@ public class FindLoopTest {
         int expect = -1;
         assertThat(result, is(expect));
     }
-
-    @Test
-    public void whenFind3() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 2, 10, 2, 4};
-        int value = 2;
-        int start = 2;
-        int finish = 4;
-        int result = find.indexOf(input, value, start, finish);
-        int expect = 3;
-        assertThat(result, is(expect));
-    }
-
-    @Test
-    public void whenSort5() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {3, 4, 1, 2, 5};
-        int result[] = find.sort(input);
-        int[] expect = new int[] {1, 2, 3, 4, 5};
-        assertThat(result, is(expect));
-    }
-}*/
+}
