efa8d06475ea1a693a2197d8713059b8933805d8
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2020-03-15 17:39:55 +0300

    добавлен классы Jar и Battery

---
+++ chapter_001/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,26 @@
+package ru.job4j.oop;
+/**
+ * @author Dmitry Kupriyanyuk (kupriyanyuk.d@yandex.ru)
+ * @version $Id$
+ * @since 09.03.19 */
+public class Battery {
+    private int load;
+
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load - another.load;
+        another.load = another.load + this.load;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(10);
+        Battery second = new Battery(5);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        first.exchange(second);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+
+    }
+}
