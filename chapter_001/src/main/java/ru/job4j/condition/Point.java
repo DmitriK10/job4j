a1e5b98b2526262357c36f26b687cce72e07691d
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2020-03-15 18:04:13 +0300

    изменён коасс Point 2.1. Рефакторинг - Расстояние между точками. [#122587]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,10 +1,41 @@
 package ru.job4j.condition;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 /**
  * @author Dmitry Kupriyanyuk (kupriyanyuk.d@yandex.ru)
  * @version $Id$
- * @since 11.03.18
- */
+ * @since 15.03.19
+ * ООП вариант
+ * */
 public class Point {
+
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+    }
+}
+
+/*
+package ru.job4j.condition;
+
+public class Point {
+
     private int x;
     private int y;
     public Point(int x, int y) {
@@ -26,4 +57,4 @@ public class Point {
         double result = a.distanceTo(b);
         System.out.println("Расстояние между точками А и В : " + result);
     }
-}
+}*/
