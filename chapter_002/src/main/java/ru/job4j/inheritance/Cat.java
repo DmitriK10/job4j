49200f5a7a5e1c800f17ea861fd5492ace7e1ac6
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2020-03-29 20:56:07 +0300

    длобалены классы Cat & ToyCat

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/Cat.java
@@ -0,0 +1,12 @@
+package ru.job4j.inheritance;
+/**
+ *Class Cat
+ *@author DKupriyanyuk
+ *@version $ld$
+ *@since 29.03.2020
+ */
+public class Cat {
+    public boolean canPurr() {
+        return true;
+    }
+}
