9fe5ec41d25e2df532288b1c3efefec66b51022f
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2020-04-13 20:47:24 +0300

    2. Перегрузка конструктора. [#173890]   вариант из комментариев Animal & Predator

--- chapter_002/src/main/java/ru/job4j/tracker/Animal.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Animal.java
@@ -2,21 +2,15 @@ package ru.job4j.tracker;
 
 public class Animal {
 
-/*    public Animal() {
-        super();
-        System.out.println("Class Animal");
-    }
-
-    public static void main(String[] args) {
-        Predator predator = new Predator();
-    }*/
+    private String name;
 
     public Animal() {
-        super();
         System.out.println("Class Animal");
     }
 
     public Animal(String name) {
+        this.name = name;
+        System.out.println("Class Animal name " + name);
     }
 
     public static void main(String[] args) {
--- chapter_002/src/main/java/ru/job4j/tracker/Predator.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -2,25 +2,18 @@ package ru.job4j.tracker;
 
 public class Predator extends Animal {
 
-/*
-    public Predator() {
-        super();
-        System.out.println("Class Predator");
-    }
-
-    public static void main(String[] args) {
-        Predator tiger = new Tiger();
-    }
- */
     public Predator() {
         super();
         System.out.println("Class Predator");
     }
 
     public Predator(String name) {
+        super();
+        System.out.println("Class Predator " + name);
     }
 
     public static void main(String[] args) {
         Predator predator = new Predator();
+        Predator predator1 = new Predator("Dog");
     }
 }
