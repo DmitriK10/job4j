1ca8d2b40ded4d18b9bdf0b5e559d3b03141fb40
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2020-04-21 21:58:39 +0300

    2. Перегрузка конструктора. [#173890]  внесение изменений по комментариям

--- chapter_002/src/main/java/ru/job4j/tracker/Predator.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -7,14 +7,14 @@ package ru.job4j.tracker;
  * @since 18.04.20
  */
 public class Predator extends Animal {
-    String name2;
+    //String name2;
 
     public Predator() {
         super();
         System.out.println("Class Predator");
     }
 
-    public Predator(String name, String name2) {
+    public Predator(String name) { //}, String name2) {
         super(name);
         this.name2 = name2;
         System.out.println("Class Predator hoot " + name2);
--- chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -14,15 +14,15 @@ public class Tiger extends Predator {
         System.out.println("Class Tiger");
     }
 
-    public Tiger(String name, String name2, String name3) {
-        super(name, name2);
+    public Tiger(String name) { //}, String name2, String name3) {
+        super(name);
         this.name3 = name3;
         System.out.println("Class Tiger roar " + name3);
     }
 
     public static void main(String[] args) {
         Tiger tiger = new Tiger();
-        Tiger tiger2 = new Tiger("", "Woo-woo","R-r-r");
+        Tiger tiger2 = new Tiger("", "R-r-r");
     }
 }
 
