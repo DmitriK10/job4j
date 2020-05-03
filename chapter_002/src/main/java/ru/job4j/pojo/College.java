eebd50a32c84438adffbe25b7ea29c00738514a1
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2020-05-03 19:37:01 +0300

    2. Модель данных. [#174683] решение задания

---
+++ chapter_002/src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,14 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setsFIO("Ivanov Ivan Ivanovitch");
+        student.setGroupID("M01");
+        student.setEnter(new Date());
+
+        System.out.println(student.getsFIO() + " has entered  - " + student.getGroupID() + " : " + student.getEnter());
+    }
+}
