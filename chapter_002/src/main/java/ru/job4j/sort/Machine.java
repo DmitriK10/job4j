114414529db74ad9ed407eef5a48cf3677fec8ce
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2020-08-22 00:02:59 +0300

    6. Сдача в кофе машине [#34741] add Machine and MachineTest

---
+++ chapter_002/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,32 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+/**
+ *Class Machine
+ *@author DKupriyanyuk
+ *@version 1
+ *@since 21.08.2020
+ */
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size;
+        size = 0;
+        int ch = 0;
+        int change = money - price;
+        while (change > 0) {
+            for (int i = 0; i <= COINS.length - 1; i++) {
+                while (COINS[i] <= change) {
+                    size++;
+                    change = change - COINS[i];
+                    rsl[ch] = COINS[i];
+                    ch++;
+                }
+               }
+        }
+
+        return Arrays.copyOf(rsl, size);
+    }
+}
