69b0dd4537a3450342012e273fca8bee94bd25c8
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2020-10-01 19:11:06 +0300

    6. Почему наследование - это плохо [#310013][#354462]

---
+++ chapter_002/src/main/java/ru/job4j/oop/LiquidationProduct.java
@@ -0,0 +1,57 @@
+package ru.job4j.oop;
+
+public class LiquidationProduct {
+    private Product product;
+
+    public LiquidationProduct(String name, int price) {
+
+        product = new Product(name, price);
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+/*    public String label() {
+        return product.label();
+    }*/
+}
+
+/*
+package ru.job4j.oop;
+
+public class LiquidationProduct extends Product {
+    public LiquidationProduct(String name, int price) {
+        super(name, price);
+    }
+
+    @Override
+    public int discount() {
+        return 90;
+    }
+}*/
+
+/*package ru.job4j.oop;
+
+public class LiquidationProduct extends Product {
+    private String name;
+
+    public LiquidationProduct(String name, int price) {
+        super(name, price);
+        this.name = name;
+    }
+
+    @Override
+    public int discount() {
+        return 90;
+    }
+
+    @Override
+    public String label() {
+        return name + " " + price();
+    }
+}*/
