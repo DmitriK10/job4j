a1f8801a8b774693d2ce104774db824e6cb61c39
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2021-01-12 19:15:21 +0300

    3. Интерфейс Input [#181092] add  Input.java, StubInput.java    - lessons example      Bus.java, Transport.java      - the task

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,16 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void ride() {
+    }
+
+    @Override
+    public void passengers(int quant) {
+    }
+
+    @Override
+    public float fill(float fuel) {
+        return 0;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.poly;
+
+public interface Transport {
+    void ride();
+
+    void passengers(int quant);
+
+    float fill(float fuel);
+}
