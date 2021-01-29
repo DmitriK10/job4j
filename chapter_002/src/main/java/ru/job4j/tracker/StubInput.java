fb00c9e3f428a01e370fef6ef008af0a1872ad9f
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2021-01-29 22:19:53 +0300

    5. Input и полиморфизм. [#181094]    StartUI.java, Stubinput.java modified files    StartUITest.java add file

--- chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -1,5 +1,25 @@
 package ru.job4j.tracker;
+//5. Input и полиморфизм. [#181094]
+public class StubInput implements Input {
+    private String[] answers;
+    private int position = 0;
 
+    public StubInput(String[] answers) {
+        this.answers = answers;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return answers[position++];
+    }
+
+    @Override
+    public int askInt(String question) {
+        return Integer.parseInt(askStr(question));
+    }
+}
+
+/*
 public class StubInput implements Input{
     @Override
     public String askStr(String question) {
@@ -11,3 +31,4 @@ public class StubInput implements Input{
         return 0;
     }
 }
+*/
