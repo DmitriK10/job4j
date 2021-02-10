dc4b6d156237be2dac27c3642e23c6072bf170ba
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2021-02-10 15:07:06 +0300

    0.3. Тестирование исключений с junit [#219366]
     Fact.java, FactTest.java, CountTest.java - modified files

--- chapter_002/src/main/java/ru/job4j/ex/Fact.java
+++ chapter_002/src/main/java/ru/job4j/ex/Fact.java
@@ -2,19 +2,14 @@ package ru.job4j.ex;
 //0.2. Кидаем исключение - throw new RuntimeException [#219365]
 
 public class Fact {
-    public static void main(String[] args) {
-
-    }
-
     public int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("Start should be less then finish.");
-        } else {
-            int rsl = 1;
-            for (int index = 1; index <= n; index++) {
-                rsl *= index;
-            }
-            return rsl;
+            throw new IllegalArgumentException("N could not be less then 0");
         }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
     }
 }
---
+++ chapter_002/src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.ex;
+//0.3. Тестирование исключений с junit [#219366]
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FactTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void when5() {
+        Fact fast = new Fact();
+        int x = fast.calc(5);
+        assertThat(x, is(120));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void when0() {
+        Fact fast = new Fact();
+        int x = fast.calc(0);
+        assertThat(x, is(1));
+    }
+}
