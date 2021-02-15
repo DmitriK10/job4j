cf0618010d521d32d309521f07688823fef8d802
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2021-02-15 14:35:11 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370]
      FactRec.java - add files

---
+++ chapter_002/src/main/java/ru/job4j/ex/FactRec.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+//0.6. Error - исключения, связанные с работой виртуальной машины [#219370]
+
+public class FactRec {
+    public static int calc(int n) {
+        if ((n == 0)|(n == 1)) {
+            return 1;
+        } else {
+            return calc(n - 1) * n;
+        }
+    }
+
+    public static void main(String[] args) {
+        int rsl = calc(3);
+        System.out.println(rsl);
+    }
+}
+
