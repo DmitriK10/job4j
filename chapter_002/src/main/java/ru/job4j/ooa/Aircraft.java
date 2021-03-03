3fdd93495f9be03fadf8fc9317ab7241783b624d
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2021-03-03 16:17:55 +0300

    4. final [#334073]
      Main.java, User.java - lessons examples
      Airbus.java, Aircraft.java, Airport.java - task

---
+++ chapter_002/src/main/java/ru/job4j/ooa/Aircraft.java
@@ -0,0 +1,9 @@
+package ru.job4j.ooa;
+
+//4. final [#334073]
+public class Aircraft {
+
+    public void printModel() {
+        System.out.println("Метод выводит в консоль модель самолета.");
+    }
+}
