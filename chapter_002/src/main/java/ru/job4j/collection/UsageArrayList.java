32161ce3c137d04c94192ada08019e530d0c1d17
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2021-06-22 16:09:10 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094]

--- chapter_002/src/main/java/ru/job4j/collection/UsageArrayList.java
+++ chapter_002/src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 
 public class UsageArrayList {
     public static void main(String[] args)  {
-        ArrayList<String> names = new ArrayList<String>();
+        ArrayList<String> names = new ArrayList<>();
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
-        for (Object value : names) {
-            System.out.println(value);
+        for (String name : names) {
+            System.out.println(name);
         }
     }
 }
