337e74175f6c16a208f17f4536232f89c266b7fa
Вьшекшн ЛÐDmitriy Kupriyanyuk <ymd@yandex.ru>
2021-07-28 20:14:59 +0300

    1. Паспорт и жители [#212656]

---
+++ chapter_002/src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+//1. Паспорт и жители [#212656]
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if ( ! citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+        //  citizens.put(citizen.getUsername(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+
+    public Citizen get(String passport) {
+        return citizens.get(passport);
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+//1. Паспорт и жители [#212656]
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void addTwo() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Ivan Sidoroff");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(citizen2);
+        assertThat(office.get(citizen.getPassport()), is(citizen2));
+    }
+
+}
